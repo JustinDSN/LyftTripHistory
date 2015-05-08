f7f82e81e7f7b5a43f2a3c5004d2ec48624089c3
Justin Steffen <JustinDSN@gmail.com>
2015-05-08 10:26:07 -0700

    Improve date formatter.

--- LyftTripHistory/LTHTrip.m
+++ LyftTripHistory/LTHTrip.m
@@ -8,7 +8,20 @@
 
 #import "LTHTrip.h"
 
+//Cache date formatter for efficiency
+static NSDateFormatter *sDateFormatter;
+
 @implementation LTHTrip
+
++ (void)initialize
+{
+    if (!sDateFormatter) {
+        sDateFormatter = [[NSDateFormatter alloc] init];
+        sDateFormatter.dateFormat = @"h:mma";
+        sDateFormatter.AMSymbol = [sDateFormatter.AMSymbol lowercaseString];
+        sDateFormatter.PMSymbol = [sDateFormatter.PMSymbol lowercaseString];
+    }
+}
 
 - (instancetype)initWithFirstLocation:(CLLocation *)location
 {
@@ -32,19 +45,15 @@
 
 - (NSString *)durationDescription
 {
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    dateFormatter.dateStyle = NSDateFormatterNoStyle;
-    dateFormatter.timeStyle = NSDateFormatterMediumStyle;
-    
-    NSString *firstLocationTime = [dateFormatter stringFromDate:self.firstLocation.timestamp];
-    NSString *lastLocationTime = [dateFormatter stringFromDate:self.lastLocation.timestamp];
+    NSString *firstLocationTime = [sDateFormatter stringFromDate:self.firstLocation.timestamp];
+    NSString *lastLocationTime = [sDateFormatter stringFromDate:self.lastLocation.timestamp];
     
 //    NSTimeInterval timeInterval = [self.lastLocation.timestamp timeIntervalSinceDate:self.firstLocation.timestamp];
 //    NSInteger hours = (int)interval / 3600;             // integer division to get the hours part
 //    NSInteger minutes = (interval - (hours*3600)) / 60; // interval minus hours part (in seconds) divided by 60 yields minutes
 //    NSString *timeDiff = [NSString stringWithFormat:@"%d:%02d", hours, minutes];
     
-    NSString *durationDescription = [NSString stringWithFormat:@"%@ - %@", firstLocationTime, lastLocationTime];
+    NSString *durationDescription = [NSString stringWithFormat:@"%@-%@", firstLocationTime, lastLocationTime];
     
     return durationDescription;
 }
